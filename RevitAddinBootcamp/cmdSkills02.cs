14525437495c6617196a34dc4f24553dbd2d3bb1
EddieS <eduardo.saez@ausco.com.au>
2025-04-23 14:10:17 +1000

    Skills02

--- RevitAddinBootcamp/cmdSkills02.cs
+++ RevitAddinBootcamp/cmdSkills02.cs
@@ -11,12 +11,36 @@
             Document doc = uidoc.Document;
 
             // Your Module 02 Skills code goes here
-            // Delete the TaskDialog below and add your code
-            TaskDialog.Show("Module 02 Skills", "Got Here!");
+            //IList<Element> PickElementsByRectangle(string statusPrompt);
+            IList<Element> picklist = uidoc.Selection.PickElementsByRectangle("Select Element by Rectangle Selection");
 
+            TaskDialog.Show("Test", "I Selected " + picklist.Count.ToString() + " Elements");
 
+            // Filter selected elements
+            List<CurveElement> allCurves = new List<CurveElement>();
+            foreach (Element elem in picklist)
+            {
+                if (elem is CurveElement)
+                {
+                    allCurves.Add(elem as CurveElement);   //(CurveElement) elem);
+                }
+            }
+
+            // Filter Selected Element for Model curves
+            List<CurveElement> modelCurves = new List<CurveElement>();
+            foreach (Element elem2 in picklist)
+            {
+                if (elem2 is CurveElement)
+                {
+                    CurveElement curveElem = (CurveElement) elem2;
+
+                    if(curveElem.CurveElementType == CurveElementType.ModelCurve)
+                    {
+                        modelCurves.Add(curveElem);
+                    }
+                }
+            }
             return Result.Succeeded;
         }
     }
-
 }
